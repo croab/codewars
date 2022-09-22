a7b298d0eb6b71706bfe28a6178c24688f3fa44c
Benjamin Croad <benjamincroadapple@gmail.com>
2022-09-22 12:54:41 +0100

    Changed names

---
+++ multithreading/lib/supermarket_queue.rb
@@ -0,0 +1,31 @@
+def queue_time(customers, n)
+  # Initialize number of customers and time that has passed (the goal of the exercise is to return the time)
+  number_of_customers = customers.size
+  time = 0
+  # Will reduce the number of customers by 1 when their time reaches 0
+  while number_of_customers > 0
+    # If n checkouts > the number of customers we do not need the additional checkouts
+    n > number_of_customers ? n = number_of_customers : n
+    # Pick first two customers (because will delete each time a customer has queued long enough)
+    first_customers = customers[0...n]
+    first_customers.each_with_index do |customer, index|
+      # Pass time for customer
+      customers[index] = customer - 1
+      # Check if the customer has finished queuing and if so delete
+      if customers[index] == 0
+        number_of_customers -= 1
+      end
+    end
+    customers.reject! { |customer| customer == 0 }
+    # Passage of time
+    time += 1
+  end
+  puts "The queue took #{time} minutes"
+  return time
+end
+
+# def handle_customers(customer, index)
+
+# end
+
+queue_time([2,2,3,3,4,4], 2)
