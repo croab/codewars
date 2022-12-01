30e4aeb95605c37d87130e1b25fd3c18dbcec18f
Benjamin Croad <benjamincroadapple@gmail.com>
2022-12-01 12:53:15 +0000

    Solution to exercise

--- typescript_duplicate_encode/src/index.ts
+++ typescript_duplicate_encode/src/index.ts
@@ -1,17 +1,18 @@
-import { workerData } from "worker_threads";
-
 function duplicateEncode(word: string){
   const wordLower: string = word.toLowerCase();
   interface Letters {
     [key: string]: number
   }
-  let newWord: string = "";
+  let newWord: Array<string> = [];
   let letters: Letters = {};
   for (let i = 0; i < wordLower.length; i++) {
     letters[wordLower.charAt(i)] ? letters[wordLower.charAt(i)] += 1 : letters[wordLower.charAt(i)] = 1;
-    letters[wordLower.charAt(i)] > 1 ? newWord.charAt(i) = '(' : newWord.charAt(i) = ')';
   }
-  return letters;
+  for (let i = 0; i < wordLower.length; i++) {
+    letters[wordLower.charAt(i)] > 1 ? newWord[i] = ')' : newWord[i] = '(';
+  }
+  return newWord.join('');
+  // return letters;
 }
 
 console.log(duplicateEncode('Hello there'));
