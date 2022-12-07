f7144135e757514585f36acc6371ba0b932108ca
Benjamin Croad <benjamincroadapple@gmail.com>
2022-12-07 16:03:06 +0000

    Next attempt at solution

--- ruby_string_incrementer/string_incrementer.rb
+++ ruby_string_incrementer/string_incrementer.rb
@@ -1,27 +1,44 @@
 def increment_string(input)
-  input_array = input.split('')
-  input_array_reversed = input_array.reverse
+  input_array_reversed = input.split('').reverse
+
+  # Initialize
   numbers_array_reversed = []
   remaining_char_reversed = []
+
+  # Loop over reversed input array to pick out trailing numbers
   input_array_reversed.each_with_index do |character, index|
     if index == 0 && !character.match(/^(\d)+$/)
       return input + '1'
     elsif character.match(/^(\d)+$/)
-      puts character
       numbers_array_reversed.push(character)
     else
       remaining_char_reversed.push(character)
     end
   end
-  num_array_length = numbers_array_reversed.length
-  final_number = numbers_array_reversed.reverse.join('').to_i
-  final_number += 1
-  num_leading_zeroes = num_array_length - final_number.digits.count
-  leading_zeroes = '0' * num_leading_zeroes
+
+  # Determine new final number (irrespective of leading zeroes)
+  final_number = numbers_array_reversed.reverse.join('').to_i + 1
+  # Determine number of leading zeroes (removed by Ruby)
+  leading_zeroes = num_leading_zeroes(numbers_array_reversed.reverse)
+  # Convert final number to string
   final_number_string = final_number.to_s
+  # Add leading zeroes
   final_number_string.insert(0, leading_zeroes)
-  remaining_char = remaining_char_reversed.reverse.join('')
-  return remaining_char + final_number_string
+  # Obtain the rest of the word as string
+  remaining_char_string = remaining_char_reversed.reverse.join('')
+  # Combine the two!
+  return remaining_char_string + final_number_string
 end
 
-puts increment_string('foobar002')
+def num_leading_zeroes(numbers_array)
+  num_zeroes = 0
+  numbers_array.each do |elem|
+    if elem == '0'
+      num_zeroes += 1
+    end
+  end
+  num_zeroes -= 1 if num_zeroes == numbers_array.size
+  return '0' * num_zeroes
+end
+
+puts increment_string('foobar00')
