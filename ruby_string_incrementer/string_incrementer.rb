0ad892a08a3853d666d701c0a39a9266a6bc8df4
Benjamin Croad <benjamincroadapple@gmail.com>
2022-12-07 15:37:21 +0000

    Another solution still with std error

--- ruby_string_incrementer/string_incrementer.rb
+++ ruby_string_incrementer/string_incrementer.rb
@@ -7,16 +7,21 @@ def increment_string(input)
     if index == 0 && !character.match(/^(\d)+$/)
       return input + '1'
     elsif character.match(/^(\d)+$/)
+      puts character
       numbers_array_reversed.push(character)
     else
       remaining_char_reversed.push(character)
     end
   end
+  num_array_length = numbers_array_reversed.length
   final_number = numbers_array_reversed.reverse.join('').to_i
   final_number += 1
+  num_leading_zeroes = num_array_length - final_number.digits.count
+  leading_zeroes = '0' * num_leading_zeroes
+  final_number_string = final_number.to_s
+  final_number_string.insert(0, leading_zeroes)
   remaining_char = remaining_char_reversed.reverse.join('')
-  return remaining_char + final_number.to_s
+  return remaining_char + final_number_string
 end
 
-puts increment_string('foo')
-puts increment_string('foo24')
+puts increment_string('foobar002')
