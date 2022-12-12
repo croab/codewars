f8d4e0925a85888857c992432c9ba6b2e51f15fe
Benjamin Croad <benjamincroadapple@gmail.com>
2022-12-12 15:25:21 +0000

    Components of time for exercise complete

---
+++ ruby_time_format/time_format.rb
@@ -0,0 +1,18 @@
+def format_duration(seconds)
+  if (seconds == 0)
+    return 'now'
+  elsif seconds > 0
+    years = seconds / (60 * 60 * 24 * 365)
+    remaining_seconds = seconds - (years * 60 * 60 * 24 * 365)
+    days = remaining_seconds / (60 * 60 * 24)
+    remaining_seconds = remaining_seconds - (days * 60 * 60 * 24)
+    hours = remaining_seconds / (60 * 60)
+    remaining_seconds = remaining_seconds - (hours * 60 * 60)
+    minutes = remaining_seconds / 60
+    seconds = remaining_seconds - (minutes * 60)
+
+    puts "#{years} years, #{days} days, #{hours} hours, #{minutes} minutes and #{seconds} seconds"
+  end
+end
+
+format_duration(10004343)
