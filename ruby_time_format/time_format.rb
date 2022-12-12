ac08ced9f4c8c8accc8289028246ba552b589315
Benjamin Croad <benjamincroadapple@gmail.com>
2022-12-12 15:35:11 +0000

    Components of time for exercise complete

--- ruby_time_format/time_format.rb
+++ ruby_time_format/time_format.rb
@@ -10,8 +10,10 @@ def format_duration(seconds)
     remaining_seconds = remaining_seconds - (hours * 60 * 60)
     minutes = remaining_seconds / 60
     seconds = remaining_seconds - (minutes * 60)
+    time_array = [years, days, hours, minutes, seconds].reject {|x| x == 0}
 
-    puts "#{years} years, #{days} days, #{hours} hours, #{minutes} minutes and #{seconds} seconds"
+    # puts "#{years} years, #{days} days, #{hours} hours, #{minutes} minutes and #{seconds} seconds"
+    time_array.each do
   end
 end
 
