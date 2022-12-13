fde5d36e453b5659250956f71d412d5fa685be01
Benjamin Croad <benjamincroadapple@gmail.com>
2022-12-13 10:30:09 +0000

    Solution to exercise

--- ruby_time_format/time_format.rb
+++ ruby_time_format/time_format.rb
@@ -2,19 +2,32 @@ def format_duration(seconds)
   if (seconds == 0)
     return 'now'
   elsif seconds > 0
-    years = seconds / (60 * 60 * 24 * 365)
-    remaining_seconds = seconds - (years * 60 * 60 * 24 * 365)
-    days = remaining_seconds / (60 * 60 * 24)
-    remaining_seconds = remaining_seconds - (days * 60 * 60 * 24)
-    hours = remaining_seconds / (60 * 60)
-    remaining_seconds = remaining_seconds - (hours * 60 * 60)
-    minutes = remaining_seconds / 60
-    seconds = remaining_seconds - (minutes * 60)
-    time_array = [years, days, hours, minutes, seconds].reject {|x| x == 0}
+    # Assign time values
+    time_hash = {
+      years: seconds / (60 * 60 * 24 * 365),
+      days: seconds / (60 * 60 * 24) % 365,
+      hours: seconds / (60 * 60) % 24,
+      minutes: seconds / 60 % 60,
+      seconds: seconds % 60
+    }
 
-    # puts "#{years} years, #{days} days, #{hours} hours, #{minutes} minutes and #{seconds} seconds"
-    time_array.each do
+    sentence_array = []
+    time_hash.each_with_index do |(time_period, value), index|
+      if time_hash.size == 1
+        phrase = "#{value} #{value == 1 ? time_period.to_s.chop : time_period.to_s}"
+        sentence_array << phrase
+        break
+      elsif index == time_hash.size - 2
+        phrase = "#{value} #{value == 1 ? time_period.to_s.chop : time_period.to_s} "
+        sentence_array << phrase
+      elsif index == time_hash.size - 1
+        phrase = "and #{value} #{value == 1 ? time_period.to_s.chop : time_period.to_s}"
+        sentence_array << phrase
+      else
+        phrase = "#{value} #{value == 1 ? time_period.to_s.chop : time_period.to_s}, "
+        sentence_array << phrase
+      end
+    end
+    return sentence_array.join('')
   end
 end
-
-format_duration(10004343)
