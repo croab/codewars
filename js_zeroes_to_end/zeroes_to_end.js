d8c906221ac376e40910c35be39370943ea3985f
Benjamin Croad <benjamincroadapple@gmail.com>
2022-12-19 15:32:45 +0000

    Solution

--- js_zeroes_to_end/zeroes_to_end.js
+++ js_zeroes_to_end/zeroes_to_end.js
@@ -1,12 +1,13 @@
 function moveZeros(arr) {
-  arr.forEach((value, index) => {
-    if (parseInt(value, 10) === 0) {
-    // if (value === 0) {
-      arr.splice(index, 1)
-      arr.push(value);
-    }
-  });
-  return arr;
+  without_zeroes = arr.filter(value => value !== 0);
+  console.log(without_zeroes);
+  zeroes = arr.filter(value => value === 0);
+  console.log(zeroes);
+
+  combined = without_zeroes.concat(zeroes);
+  console.log(combined);
+
+  return combined;
 }
 
-console.log(moveZeros([ 1, [], [], [], 4, '9', {}, {}, [], 1, '0' ]));
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]));
