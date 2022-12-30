e881be4c99965d00a609f0c3da7570369540577d
Benjamin Croad <benjamincroadapple@gmail.com>
2022-12-30 13:04:43 +0000

    Works but slow

--- ruby_smaller_same_digits/smaller_same_digits.rb
+++ ruby_smaller_same_digits/smaller_same_digits.rb
@@ -1,26 +1,21 @@
 def next_smaller(n)
-  digits_array = n.to_s.split("").map {|str| str.to_i}
-  sorted_digits_array = digits_array.sort_by { |digit| -digit}
-  possible_first_digits = sorted_digits_array.select { |digit| digit <= digits_array[0] }
-  possible_first_digits.each_with_index do |first_digit, index|
-    if (index == 0)
-      copied_digits_array = digits_array.dup
-      copied_digits_array.insert(-2, copied_digits_array.delete_at(-1))
-      number_attempt = copied_digits_array.join.to_i
-      return number_attempt if number_attempt < n
-    else
-      copied_sorted_digits_array = sorted_digits_array.dup
-      index_of_first_digit = copied_sorted_digits_array.find_index(first_digit)
-      copied_sorted_digits_array.delete_at(index_of_first_digit)
-      copied_sorted_digits_array.unshift(first_digit)
-      number_attempt = copied_sorted_digits_array.join.to_i
-      return number_attempt if number_attempt < n
-    end
-  end
-  return -1
+  digits_array = n.to_s.split("")
+  combinations = digits_array.permutation(digits_array.size).to_a
+  combinations.map! { |array| array.join.to_i }
+  possible_numbers = combinations.filter { |number| number < n}
+  sorted_combinations = possible_numbers.sort_by { |number| -number}
+  return -1 if sorted_combinations.size == 0
+  return -1 if sorted_combinations[0].to_s.split("").size < digits_array.size
+  return sorted_combinations[0]
 end
-
+p "Expecting 513"
 p next_smaller(531)
 # == 513
+p "Expecting 2017"
 p next_smaller(2071)
 # == 2017
+p "Expecting -1"
+p next_smaller(1027)
+
+p "Expecting 144"
+p next_smaller(414)
