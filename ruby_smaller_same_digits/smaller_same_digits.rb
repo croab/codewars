1ffb794055325ce38d3c63c17daceed2dcaf4202
Benjamin Croad <benjamincroadapple@gmail.com>
2022-12-30 11:32:36 +0000

    Solution start

--- ruby_smaller_same_digits/smaller_same_digits.rb
+++ ruby_smaller_same_digits/smaller_same_digits.rb
@@ -2,21 +2,25 @@ def next_smaller(n)
   digits_array = n.to_s.split("").map {|str| str.to_i}
   sorted_digits_array = digits_array.sort_by { |digit| -digit}
   possible_first_digits = sorted_digits_array.select { |digit| digit <= digits_array[0] }
-  new_number = n
-  # For each in possible_first_digits
-  possible_first_digits.each do |first_digit|
-    copied_sorted_digits_array = sorted_digits_array.dup
-    index_of_first_digit = copied_sorted_digits_array.find_index(first_digit)
-    copied_sorted_digits_array.delete_at(index_of_first_digit)
-    p copied_sorted_digits_array
+  possible_first_digits.each_with_index do |first_digit, index|
+    if (index == 0)
+      copied_digits_array = digits_array.dup
+      copied_digits_array.insert(-2, copied_digits_array.delete_at(-1))
+      number_attempt = copied_digits_array.join.to_i
+      return number_attempt if number_attempt < n
+    else
+      copied_sorted_digits_array = sorted_digits_array.dup
+      index_of_first_digit = copied_sorted_digits_array.find_index(first_digit)
+      copied_sorted_digits_array.delete_at(index_of_first_digit)
+      copied_sorted_digits_array.unshift(first_digit)
+      number_attempt = copied_sorted_digits_array.join.to_i
+      return number_attempt if number_attempt < n
+    end
   end
-  # Try keeping first number
-  # Try to make a lower number using the remaining digits
-  # If this doesn't work, find next highest number for position 1
-  return possible_first_digits
+  return -1
 end
 
-next_smaller(531)
+p next_smaller(531)
 # == 513
-next_smaller(2071)
+p next_smaller(2071)
 # == 2017
